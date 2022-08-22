4237054d598fdd229c1871615bb4c198d005fb34
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2022-08-22 13:01:54 +0100

    added media to gitignore

--- linktree/settings.py
+++ linktree/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
                  "app-linktree.herokuapp.com", "0.0.0.0"]
 
