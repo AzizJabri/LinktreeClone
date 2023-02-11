5f9b9d0648b9850f586e8a28d36dae46d849554c
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2023-02-11 01:26:26 +0100

    quick security issue

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import random
 from django.views.generic.edit import FormView
 from django.views.generic.base import TemplateView
 from django.views.decorators.debug import sensitive_post_parameters
@@ -95,7 +96,7 @@ class RegisterView(View):
                 user=user)
             profile.save()
             profile.username = slugify(
-                user.first_name + user.last_name + str(profile.id))
+                user.first_name + user.last_name + random.randint(1, 99))
             profile.save()
             login(request, user)
             messages.success(
